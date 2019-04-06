00a25a370782f7bc10f6e168a00d3a195f2e5b32
nameverb <drew.stanleymso@gmail.com>
2019-04-06 00:00:04 -0400

    reverted json file back to results

--- psychology_today.py
+++ psychology_today.py
@@ -33,31 +33,32 @@ for a_id in all_entries:
     entry_request = requests.get(abs_entry, headers=headers)
     entry_html = entry_request.text
     entry_soup = BeautifulSoup(entry_html, "html.parser")
-
-    all_labels = soup.find_all("div", attrs = {'class':'listing-profile'})
-    for a_div in all_labels:
-# print('------------')
-# print(a_div.text)
-        alink = a_div.find('a')
-        abs_url = alink['href'].split("?")[0]
-# print(abs_url)
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-            }
-
-        item_request = requests.get(abs_url, headers=headers)
-        item_html = item_request.text
-        item_soup = BeautifulSoup(item_html, "html.parser")
-# print(item_soup)
-
-        address = item_soup.find("div", attrs = {'itemprop' : 'address'})
-        # print(address)
-        time.sleep(5)
-        alabama_address = {}
-        alabama_address["address"] = address.text
-        alabama_address["url"] = abs_url
-        all_my_data.append(alabama_address)
-    json_file = open('alabama.json','w')
-    json.dump(all_my_data,json_file, indent=4)
-    json_file.close()
+    print(entry_soup)
+#
+#     all_labels = soup.find_all("div", attrs = {'class':'listing-profile'})
+#     for a_div in all_labels:
+# # print('------------')
+# # print(a_div.text)
+#         alink = a_div.find('a')
+#         abs_url = alink['href'].split("?")[0]
+# # print(abs_url)
+#
+#         headers = {
+#             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+#             }
+#
+#         item_request = requests.get(abs_url, headers=headers)
+#         item_html = item_request.text
+#         item_soup = BeautifulSoup(item_html, "html.parser")
+# # print(item_soup)
+#
+#         address = item_soup.find("div", attrs = {'itemprop' : 'address'})
+#         # print(address)
+#         time.sleep(5)
+#         alabama_address = {}
+#         alabama_address["address"] = address.text
+#         alabama_address["url"] = abs_url
+#         all_my_data.append(alabama_address)
+#     json_file = open('alabama.json','w')
+#     json.dump(all_my_data,json_file, indent=4)
+#     json_file.close()
