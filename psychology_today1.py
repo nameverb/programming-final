7eeb12c7ce22aac14597912968a33a07cfab4028
nameverb <drew.stanleymso@gmail.com>
2019-04-06 15:42:48 -0400

    split not working

--- psychology_today1.py
+++ psychology_today1.py
@@ -12,19 +12,19 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 }
 
-url = "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/a"
+url = "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/"
 results_page = requests.get(url, headers=headers)
 page_html = results_page.text
 soup = BeautifulSoup(page_html, "html.parser")
 
-all_labels = soup.find_all("div", attrs = {'class':'listing-profile'})
+all_labels = soup.find("div", attrs = {'id':'AZ'})
 for a_div in all_labels:
     # print('------------')
     # print(a_div.text)
 
     alink = a_div.find('a')
-    abs_url = alink['href'].split("?")[0]
-    # print(abs_url)
+    abs_url = alink['href']
+    # print(alink)
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
@@ -33,15 +33,33 @@ for a_div in all_labels:
     item_request = requests.get(abs_url, headers=headers)
     item_html = item_request.text
     item_soup = BeautifulSoup(item_html, "html.parser")
+    # print(item_html)
+
+    all_profile = soup.find_all("div", attrs = {'class':'listing-profile'})
+    for a_profile in all_profile:
+        # print('------------')
+        # print(a_div.text)
+
+        aprof = a_profile.find('a')
+        profile_url = aprof['href']
+        # print(abs_url)
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+        }
+
+        profile_request = requests.get(profile_url, headers=headers)
+        profile_html = profile_request.text
+        profile_soup = BeautifulSoup(profile_html, "html.parser")
     # print(item_soup)
 
-    address = item_soup.find("div", attrs = {'itemprop' : 'address'})
-    # print(address)
-    time.sleep(5)
-    alabama_address = {}
-    alabama_address["address"] = address.text
-    alabama_address["url"] = abs_url
-    all_my_data.append(alabama_address)
-    json_file = open('alabama.json','w')
-    json.dump(all_my_data,json_file, indent=4)
-    json_file.close()
+        address = profile_soup.find("div", attrs = {'itemprop' : 'address'})
+        # print(address)
+        time.sleep(5)
+        alabama_address = {}
+        alabama_address["address"] = address.text
+        alabama_address["url"] = profile_url
+        all_my_data.append(alabama_address)
+        json_file = open('alabama.json','w')
+        json.dump(all_my_data,json_file, indent=3)
+        json_file.close()
