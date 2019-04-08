a5b52280c7e76a5c2ecc36faf73daf6a7fa7c07d
nameverb <drew.stanleymso@gmail.com>
2019-04-08 18:04:00 -0400

    created list of urls for Alabama alpha

--- psychology_today.py
+++ psychology_today.py
@@ -14,20 +14,18 @@ headers = {
 }
 
 therapist_alpha = re.compile("[a-z]")
-url = "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/"
+url_list = ["https://www.psychologytoday.com/us/therapists/profile-listings/alabama/a", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/b", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/c", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/d", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/e", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/f", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/g", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/h", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/i", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/j", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/k", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/l", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/m", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/n", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/o", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/p", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/q", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/r", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/s", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/t", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/u", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/v", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/w", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/y", "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/z"]
 results_page = requests.get(url, headers=headers)
 page_html = results_page.text
 soup = BeautifulSoup(page_html, "html.parser")
 
 
-all_labels = soup.find_all("div", attrs = {'id':'AZ'})
-for a_div in all_labels:
-    # print('------------')
-    # print(a_div.text)
-
+all_listings = soup.find_all("div", attrs = {'class':'prof-listing'})
+#pull all a tags out of the alphabetical list of therapists
+for a_div in all_listings:
     alink = a_div.find('a')
     abs_url = alink['href']
-    # print(abs_url)
+
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
@@ -36,18 +34,26 @@ for a_div in all_labels:
     item_request = requests.get(abs_url, headers=headers)
     item_html = item_request.text
     item_soup = BeautifulSoup(item_html, "html.parser")
-    # print(item_soup)
-    all_prof = soup.find_all("div", attrs = {'class':'listing-profile'})
-    for prof in all_prof:
-        proflink =a_div.find('a')
-        proflink
 
-        address = item_soup.find("div", attrs = {'itemprop' : 'address'})
-        # print(address)
+    #pull all listing profiles in a letter category
+    all_prof = item_soup.find_all("div", attrs = {'class':'col-9 col-sm-9 col-md-10 col-lg-10 col-xl-10 col-tight-left'})
+
+    for prof in all_prof:
+        proflink = prof.find('a')
+        profurl = proflink['href']
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+        }
+        prof_request = requests.get(profurl, headers=headers)
+        prof_html = prof_request.text
+        prof_soup = BeautifulSoup(prof_html, "html.parser")
+
+        address = prof_soup.find("div", attrs = {'itemprop' : 'address'})
         time.sleep(5)
         alabama_address = {}
         alabama_address["address"] = address.text
-        alabama_address["url"] = abs_url
+        alabama_address["url"] = profurl
         all_my_data.append(alabama_address)
         json_file = open('alabama.json','w')
         json.dump(all_my_data,json_file, indent=2)
