0e557ed99ecd986d7a4d0b4ae9e6ad627a0b7da4
nameverb <drew.stanleymso@gmail.com>
2019-04-08 15:20:00 -0400

    started adding another layer of for loop

--- psychology_today.py
+++ psychology_today.py
@@ -14,13 +14,13 @@ headers = {
 }
 
 therapist_alpha = re.compile("[a-z]")
-url = "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/ + therapist_alpha"
+url = "https://www.psychologytoday.com/us/therapists/profile-listings/alabama/"
 results_page = requests.get(url, headers=headers)
 page_html = results_page.text
 soup = BeautifulSoup(page_html, "html.parser")
 
 
-all_labels = soup.find_all("div", attrs = {'class':'listing-profile'})
+all_labels = soup.find_all("div", attrs = {'id':'AZ'})
 for a_div in all_labels:
     # print('------------')
     # print(a_div.text)
@@ -37,14 +37,18 @@ for a_div in all_labels:
     item_html = item_request.text
     item_soup = BeautifulSoup(item_html, "html.parser")
     # print(item_soup)
+    all_prof = soup.find_all("div", attrs = {'class':'listing-profile'})
+    for prof in all_prof:
+        proflink =a_div.find('a')
+        proflink
 
-    address = item_soup.find("div", attrs = {'itemprop' : 'address'})
-    # print(address)
-    time.sleep(5)
-    alabama_address = {}
-    alabama_address["address"] = address.text
-    alabama_address["url"] = abs_url
-    all_my_data.append(alabama_address)
-    json_file = open('alabama.json','w')
-    json.dump(all_my_data,json_file, indent=2)
-    json_file.close()
+        address = item_soup.find("div", attrs = {'itemprop' : 'address'})
+        # print(address)
+        time.sleep(5)
+        alabama_address = {}
+        alabama_address["address"] = address.text
+        alabama_address["url"] = abs_url
+        all_my_data.append(alabama_address)
+        json_file = open('alabama.json','w')
+        json.dump(all_my_data,json_file, indent=2)
+        json_file.close()
